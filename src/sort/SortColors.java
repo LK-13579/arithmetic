a36482b17651ae9aa69cf170481ec79c3ecd6c8a
e <lk130337>
2021-06-17 16:10:07 +0800

    2020-06-17：颜色分类问题(边界指针法)

--- src/sort/SortColors.java
+++ src/sort/SortColors.java
@@ -27,8 +27,8 @@ package sort;
 public class SortColors {
 
   public static void main(String[] args) {
-    int[] nums = {0,1,0};
-    sortColor(nums);
+    int[] nums = {1,0,2,2,1,0};
+    sortColor2(nums);
   }
 
   /**
@@ -79,20 +79,17 @@ public class SortColors {
   }
 
   public static void sortColor2(int[] nums) {
-    int n = nums.length;
-    int p0 = 0, p2 = n - 1;
-    for (int i = 0; i <= p2; ++i) {
-      while (i <= p2 && nums[i] == 2) {
-        int temp = nums[i];
-        nums[i] = nums[p2];
-        nums[p2] = temp;
-        --p2;
-      }
-      if (nums[i] == 0) {
-        int temp = nums[i];
-        nums[i] = nums[p0];
-        nums[p0] = temp;
-        ++p0;
+    int num0 = 0, num1 = 0, num2 = 0;
+    for(int i = 0; i < nums.length; i++) {
+      if(nums[i] == 0) {
+        nums[num2++] = 2;
+        nums[num1++] = 1;
+        nums[num0++] = 0;
+      }else if(nums[i] == 1) {
+        nums[num2++] = 2;
+        nums[num1++] = 1;
+      }else {
+        nums[num2++] = 2;
       }
     }
     System.out.println(nums);
